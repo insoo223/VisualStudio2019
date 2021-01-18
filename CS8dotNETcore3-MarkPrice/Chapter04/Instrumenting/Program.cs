27253189a52b4bee85773a40d35732b3dac13751
Insoo Kim <insoo@timmanage.com>
2021-01-18 13:48:49 +0900

    Trace for end-user logging

--- CS8dotNETcore3-MarkPrice/Chapter04/Instrumenting/Program.cs
+++ CS8dotNETcore3-MarkPrice/Chapter04/Instrumenting/Program.cs
@@ -8,35 +8,59 @@ namespace Instrumenting
   {
     static void Main(string[] args)
     {
-      // write to a text file in the project folder
-      Trace.Listeners.Add(new TextWriterTraceListener(
-        File.CreateText("log.txt")));
+            // write to a text file in the project folder
+            Trace.Listeners.Add(new TextWriterTraceListener(
+              File.CreateText("log.txt")));
 
-      // text writer is buffered, so this option calls
-      // Flush() on all listeners after writing
-      Trace.AutoFlush = true;
+            // text writer is buffered, so this option calls
+            // Flush() on all listeners after writing
+            Trace.AutoFlush = true;
 
-      Debug.WriteLine("Debug says, I am watching!");
-      Trace.WriteLine("Trace says, I am watching!");
+            Debug.WriteLine("Debug says, 안녕 인수!, I am watching!");
+            Trace.WriteLine("Trace says, 你好，仁秀！，I am watching!");
 
-      var builder = new ConfigurationBuilder()
-        .SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile("appsettings.json",
-          optional: true, reloadOnChange: true);
+            var builder = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json",
+                optional: true, reloadOnChange: true);
 
-      IConfigurationRoot configuration = builder.Build();
+            IConfigurationRoot configuration = builder.Build();
 
-      var ts = new TraceSwitch(
-        displayName: "PacktSwitch",
-        description: "This switch is set via a JSON config.");
+            var ts = new TraceSwitch(
+              displayName: "PacktSwitch",
+              description: "This switch is set via a JSON config.");
 
-      configuration.GetSection("PacktSwitch").Bind(ts);
+            configuration.GetSection("PacktSwitch").Bind(ts);
 
-      Trace.WriteLineIf(ts.TraceError, "Trace error");
-      Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
-      Trace.WriteLineIf(ts.TraceInfo, "Trace information");
-      Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+            Trace.WriteLineIf(ts.TraceError, "Trace error");
+            Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+            Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+            Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+        }//Main
+        public void sub2()
+        {
+            /*
+            Debug.WriteLine("Debug says, I am watching!");
+            Trace.WriteLine("Trace says, I am watching!");
 
-    }
-  }
-}
+            var builder = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json",
+                optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var ts = new TraceSwitch(
+              displayName: "PacktSwitch",
+              description: "This switch is set via a JSON config.");
+
+            configuration.GetSection("PacktSwitch").Bind(ts);
+
+            Trace.WriteLineIf(ts.TraceError, "Trace error");
+            Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+            Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+            Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+            */
+        }
+    }//Program
+}//Instrumenting
