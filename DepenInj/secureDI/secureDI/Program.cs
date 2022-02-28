a04fb91e3fd43def9fe4b7a4c8548990adc4b74b
Insoo Kim <insoo@hotmail.com>
2022-02-28 17:13:29 +0900

    hwpHelloWorld
    HWP Automation

---
+++ DepenInj/secureDI/secureDI/Program.cs
@@ -0,0 +1,74 @@
+﻿/*------------------------------------------------------------------------
+0. File: secureDIMain.cs
+1. Purpose: To show the concept of Extensibility via "Dependency Injection"
+2. Author: Insoo Kim ( insoo@timmanage.com )
+3. Created: Tue. Jan 25, 2022
+4. Updated: 
+5. How to run on Ubuntu Linux:
+  A. Install Mono dev. tool chain.
+    $ sudo apt install mono-complete
+  B. Build
+    $ mcs -out:secureDIMain.exe secureDIMain.cs
+    --> Linux built binaries can be run on Windows, 
+    --> but its file extenstion should be modified as "exe" to run on Windows.
+  C. Run on Linux or Windows
+    $ ./secureDIMain
+6. Ref
+  A. How can I compile, run and decompile C# code in Ubuntu terminal?
+  https://askubuntu.com/questions/1100537/how-can-i-compile-run-and-decompile-c-code-in-ubuntu-terminal
+  B. Install the .NET SDK or the .NET Runtime on Ubuntu
+  https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu
+------------------------------------------------------------------------*/
+using System;
+using System.Security.Principal;
+
+namespace TIM
+{
+    class EntryClass
+    {
+        private static void Main()
+        {
+            IMessageWriter writer = new ConsoleMsgWriter();
+            var salute = new Salute(writer);
+            salute.Exclaim();
+        }
+    }//EntryClass
+
+    public class Salute
+    {
+        private readonly IMessageWriter writer;
+        public Salute(IMessageWriter writer)
+        {
+            if (writer == null)
+                throw new ArgumentNullException("writer");
+
+            this.writer = writer;
+        }//constructor
+
+        public void Exclaim()
+        {
+            this.writer.Write("Hello, Dependency Injection World!");
+        }
+    }//class Salute
+
+    public interface IMessageWriter
+    {
+        void Write(string msg);
+    }//interface IMessageWriter
+
+    public class ConsoleMsgWriter : IMessageWriter
+    {
+        public void Write(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }//class ConsoleMsgWriter
+
+    public class SecureMsgWriter : IMessageWriter
+    {
+        private readonly IMessageWriter writer;
+        private readonly IIdentity identity;
+
+    }
+
+}
---
+++ hwpAuto/hwpHelloWorld/hwpHelloWorld/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace hwpHelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axHwpCtrl1 = new AxHWPCONTROLLib.AxHwpCtrl();
+            this.btnOpenHWP = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axHwpCtrl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axHwpCtrl1
+            // 
+            this.axHwpCtrl1.Enabled = true;
+            this.axHwpCtrl1.Location = new System.Drawing.Point(12, 12);
+            this.axHwpCtrl1.Name = "axHwpCtrl1";
+            this.axHwpCtrl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axHwpCtrl1.OcxState")));
+            this.axHwpCtrl1.Size = new System.Drawing.Size(960, 614);
+            this.axHwpCtrl1.TabIndex = 0;
+            // 
+            // btnOpenHWP
+            // 
+            this.btnOpenHWP.Location = new System.Drawing.Point(995, 566);
+            this.btnOpenHWP.Name = "btnOpenHWP";
+            this.btnOpenHWP.Size = new System.Drawing.Size(143, 45);
+            this.btnOpenHWP.TabIndex = 1;
+            this.btnOpenHWP.Text = "Open HWP";
+            this.btnOpenHWP.UseVisualStyleBackColor = true;
+            this.btnOpenHWP.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1156, 638);
+            this.Controls.Add(this.btnOpenHWP);
+            this.Controls.Add(this.axHwpCtrl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axHwpCtrl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxHWPCONTROLLib.AxHwpCtrl axHwpCtrl1;
+        private System.Windows.Forms.Button btnOpenHWP;
+    }
+}
+
